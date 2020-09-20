2059da7bd23a4c8432a79ac6f878cd0bd2527043
Alvin Loh <alvinloh@alvinloh.com>
2020-09-21 00:12:26 +0800

    Going to add mongoose

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,22 @@
 import {app} from './app';
 import * as http from 'http';
 import {MongoHelper} from "./mongo.helper";
-
+import mongoose, {mongo} from 'mongoose';
 
 const PORT = 8080;
+const MONGO_URI = "mongodb://192.168.50.105:27017/todo";
 const server = http.createServer(app);
 
 server.listen(PORT);
 
 server.on('listening' , async () =>{
    console.info(`Listening on ${PORT}`);
-   try{
-      await MongoHelper.connect('mongodb://192.168.50.105:27017');
-      console.info('Connected to Mongo');
-   }
-   catch(err){
+   mongoose.connect(MONGO_URI , {useNewUrlParser: true});
+   mongoose.connection.on('open', () => {
+      console.info('Connected to Mongo ');
+   });
+   mongoose.connection.on('error' , (err: any) =>{
       console.error(err);
-   }
+   });
+
 });
