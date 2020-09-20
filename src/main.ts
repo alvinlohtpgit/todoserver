0216a992ade907676357fc8567bf7f16f9825141
Alvin Loh <alvinloh@alvinloh.com>
2020-09-21 00:08:03 +0800

    Going to add mongoose

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import {app} from './app';
 import * as http from 'http';
+import {MongoHelper} from "./mongo.helper";
 
 
 const PORT = 8080;
@@ -7,6 +8,13 @@ const server = http.createServer(app);
 
 server.listen(PORT);
 
-server.on('listening' , () =>{
+server.on('listening' , async () =>{
    console.info(`Listening on ${PORT}`);
+   try{
+      await MongoHelper.connect('mongodb://192.168.50.105:27017');
+      console.info('Connected to Mongo');
+   }
+   catch(err){
+      console.error(err);
+   }
 });
---
+++ src/todo.controller.ts
@@ -0,0 +1,56 @@
+import express from "express";
+import {app} from "./app";
+import {MongoHelper} from "./mongo.helper";
+import mongodb from 'mongodb';
+
+const todoRoutes = express.Router();
+
+const getCollection = () => {
+  return MongoHelper.client.db('todo').collection('todos');
+};
+
+todoRoutes.get('/todo' , (req:express.Request , resp: express.Response, next: express.NextFunction) => {
+    const collection = getCollection();
+    collection.find({}).toArray((err , items) => {
+        if (err){
+            resp.status(500);
+            resp.end();
+            console.error('Caught error ', err);
+        }
+        else{
+            items= items.map((item) => {return {id : item._id, description: item.description}})
+            resp.json(items);
+        }
+    })
+});
+
+todoRoutes.post('/todo' , (req:express.Request , resp: express.Response, next: express.NextFunction) => {
+    const description = req.body.description;
+    console.info('Description received : ', req.body);
+    const collection = getCollection();
+    collection.insert({description:description});
+    resp.end();
+});
+
+todoRoutes.put('/todo/:id' , (req:express.Request , resp: express.Response, next: express.NextFunction) => {
+
+    const description = req.body['description'];
+    const id = req.params['id'];
+
+    const collection = getCollection();
+
+    collection.findOneAndUpdate( {"_id" : new mongodb.ObjectId(id)} , {$set: {description: description}} );
+
+    resp.end();
+});
+
+todoRoutes.delete('/todo/:id' , (req:express.Request , resp: express.Response, next: express.NextFunction) => {
+    const id = req.params['id'];
+    const collection = getCollection();
+
+    collection.remove({"_id": new mongodb.ObjectId(id)});
+    resp.end();
+});
+
+export {todoRoutes}
+
